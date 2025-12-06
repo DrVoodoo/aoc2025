08575da6eaa8c4e44a7fa896b57649e319a66a8e
kristian <kristian_rodin@hotmail.com>
2025-12-06 11:36:32 +0100

    First part of day four

---
+++ day04/src/main.rs
@@ -0,0 +1,96 @@
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
+fn main() -> io::Result<()> {
+    let mut rolls_of_paper = 0;
+    let grid = parse_file()?;
+
+    let length = grid[0].len() as i32;
+    let height = grid.len() as i32;
+
+    for y in 0..height {
+        for x in 0..length {
+            let value = grid[y as usize][x as usize];
+            
+            if value == '@' {
+                let collision_grid = get_collision_grid(&grid, x, y, length, height);
+                if can_forklift_paper_roll(collision_grid) {
+                    rolls_of_paper += 1;
+                }
+            }
+        }
+    }
+
+    println!("Total paper rolls fork lifted: {}", rolls_of_paper);
+
+    Ok(())
+}
+
+fn get_collision_grid(grid: &Vec<Vec<char>>, x: i32, y: i32, length: i32, height: i32) -> Vec<Vec<char>> {
+    let x_start: i32 = x as i32 - 1;
+    let x_end: i32 = x as i32 + 1;
+    let y_start: i32 = y as i32 - 1;
+    let y_end: i32 = y as i32 + 1;
+    let mut collision_grid: Vec<Vec<char>> = Vec::new();
+
+    for i in y_start..=y_end {
+        let mut x_row: Vec<char> = Vec::new();
+        for j in x_start..=x_end {
+            if i < 0 || j < 0 {
+                x_row.push('.');
+            }
+            else if i >= height || j >= length {
+                x_row.push('.');
+            }
+            else {
+                x_row.push(grid[i as usize][j as usize]);
+            }
+        }
+        collision_grid.push(x_row);
+    }
+
+    collision_grid
+}
+
+fn can_forklift_paper_roll(collision_grid: Vec<Vec<char>>) -> bool {
+    let mut total_paper_rolls = 0;
+    for y in collision_grid.iter() {
+        for x in y.iter() {
+            if *x == '@' {
+                total_paper_rolls += 1;
+            }
+        }
+    }
+    
+    total_paper_rolls -= 1; // remove the center one
+
+    if total_paper_rolls < 4 {
+        return true;
+    }
+
+    false
+}
+
+fn parse_file() -> io::Result<Vec<Vec<char>>> {
+    let file = File::open("day04/input.txt")?;
+    let mut reader = BufReader::new(file);
+    let mut line = String::new();
+
+    let mut values: Vec<String> = Vec::new();
+
+    // read the lines
+    while reader.read_line(&mut line)? != 0 {
+        values.push(line.trim().to_string());
+        line.clear();
+    }
+
+    // creat a structure from the lines
+    let mut grid: Vec<Vec<char>> = Vec::new();
+
+    for i in values.iter() {
+        let chars: Vec<char> = i.chars().collect();
+        grid.push(chars);
+    }   
+
+    Ok(grid)
+}
